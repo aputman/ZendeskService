32a0d90f403e9e4a28656d6e1b0c9dae984789e7
aputman <aputman@hmc.edu>
2015-09-14 22:36:44 -0700

    new

--- __init__.py
+++ __init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+emails = {"aputman@hmc.edu": {'name': 'Alex Putman', 'age': 21}}
+
 @app.route('/')
 def hello():
 	return 'Hello World!'
+
+@app.route('/api/<email>')
+def api(email = None):
+	if (email in emails):
+		return flask.jsonify(**emails[email])
+	else:
+		abort(404)
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0")
