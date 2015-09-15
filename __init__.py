35c1074a5d43fd92285bb8930f3f1b31ba4c9b89
aputman <aputman@hmc.edu>
2015-09-14 22:45:34 -0700

    d

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
 
-emails = {'aputman@hmc.edu': {'name': 'Alex Putman', 'age': 21}}
+emails = {'aputman@hmc.edu': {'name': 'Alex Putman', 'age': 21}, '': {'win': True}}
 
 @app.route('/')
 def hello():
